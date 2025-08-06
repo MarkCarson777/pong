793eb87f05f7c9df40582610b25f4b64847efbc2
Mark Carson <markcarson121@gmail.com>
2025-08-06 14:48:30 -0300

    added documentation to gameConstants

--- src/constants/gameConstants.ts
+++ src/constants/gameConstants.ts
@@ -1,3 +1,9 @@
+/** This file contains constants used throughout the Pong game application.
+ * These constants define the dimensions of the canvas, paddles, and ball,
+ * as well as the speed of the ball.
+ * They are used to ensure consistent rendering and gameplay mechanics.
+ */
+
 export const CANVAS = {
   WIDTH: 800,
   HEIGHT: 400,
